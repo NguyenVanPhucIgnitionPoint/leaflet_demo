30c3b6a8e4f46dcfe3688cea15b20929b73e0ef4
IGP-NguyenVanPhuc <vn.nguyenvan.phuc@ignitionpoint-inc.com>
2020-04-17 17:13:26 +0700

    Init map

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeafletDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿var mymap = L.map('mapid').setView([21.006966, 105.802499], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+L.Control.geocoder().addTo(mymap);
+
+L.marker([21.006966, 105.802499]).addTo(mymap).bindPopup('IGP Viet Nam').openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup.setLatLng(e.latlng)
+         .setContent("You clicked the map at " + e.latlng.toString())
+         .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
